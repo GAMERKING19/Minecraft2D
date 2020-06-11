67744df1ea58b3add858c63450a5a5382c1f1fff
GAMERKING19 <66739090+GAMERKING19@users.noreply.github.com>
2020-06-11 22:06:51 +0530

    changes

--- ShapeSurvival2D/Assets/Scripts/Advanced_Player_movement.cs
+++ ShapeSurvival2D/Assets/Scripts/Advanced_Player_movement.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Advanced_Player_movement : MonoBehaviour
 {
@@ -7,59 +6,31 @@ public class Advanced_Player_movement : MonoBehaviour
 
         public float jump_force = 5f;
 
-
         public bool is_grounded = false;
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0f,0f);
-
         transform.position += movement * Time.deltaTime * moveSpeed;
 
-
-        
-
-
-        if(Input.GetAxis("Horizontal") < 0){
-
-
+        if(Input.GetAxis("Horizontal") < 0)
+        {
             transform.eulerAngles = new Vector3(0, 0, 0);
-
-            
-
-
-
-
-
         }
 
-
-        if(Input.GetAxis("Horizontal") > 0){
-
-           
+        if(Input.GetAxis("Horizontal") > 0)
+        {
             transform.eulerAngles = new Vector3(0, 180, 0);
-
-
-
         }
-        
-
     }
 
-
-    void jump(){
-
-        if (Input.GetKey("space") && is_grounded == true){
-        
-        
-
-        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,jump_force),ForceMode2D.Impulse);
+    void jump()
+    {
+        if (Input.GetKey("space") && is_grounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump_force), ForceMode2D.Impulse);
         }
     }
 }
