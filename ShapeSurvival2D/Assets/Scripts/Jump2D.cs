0df67fd512d381550c7864320c89fb92d7e9faf5
ElectroMagneticSpectrum <tiontension21@gmail.com>
2020-06-11 22:03:12 +0530

    bug fixes and camera can now follow player
    Fixed the jumping bug and now used cinemachine to follow the player

---
+++ ShapeSurvival2D/Assets/Scripts/Jump2D.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Jump2D : MonoBehaviour
+{
+    [SerializeField] private LayerMask platform;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+   
+
+    private void Awake()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            float jumpVelocity = 6f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
+
+
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platform);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
+    }
+
+
+}
